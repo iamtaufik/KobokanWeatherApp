5fb300211d27104b4ffc02dd9621f13980c8386f
muhammadtaufikhidayat <muhtopik07@gmail.com>
2022-04-10 12:26:27 +0700

    Perbaikan UI

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 // const apiKey = 'jCLPUDFqHDZV7369qCF3gfHGutmpcVKG';
-// const apiKey = 'bRx62nQIMH72uPYxEQRTtU2d6C4Vgq1l';
-const apiKey = 'FA2WGKg0MlXXCP1SC4ugWCEVKSQi2sfW';
+const apiKey = 'bRx62nQIMH72uPYxEQRTtU2d6C4Vgq1l';
+// const apiKey = 'FA2WGKg0MlXXCP1SC4ugWCEVKSQi2sfW';
 const searchButton = document.querySelector('.search-button');
 const container = document.querySelector('.card-container');
-const popularCity = ['Bandung', 'Yogyakarta', 'Jakarta'];
 
 searchButton.addEventListener('click', async function () {
   const inputKeyword = document.querySelector('.input-keyword');
   const { key, location } = await getLocationKey(inputKeyword.value, apiKey);
-  const { weatherText, temp, icon } = await getWeatherCondition(key, apiKey);
-  container.innerHTML = dataWeather(location, weatherText, icon, temp);
+  const { weatherText, temp, icon, dataDay } = await getWeatherCondition(key, apiKey);
+  container.innerHTML = dataWeather(location, weatherText, icon, temp, dataDay);
 });
 
 async function getLocationKey(input, apiKey) {
@@ -24,23 +23,53 @@ async function getWeatherCondition(key, apiKey) {
   const response = await fetch('https://dataservice.accuweather.com/currentconditions/v1/' + key + '?apikey=' + apiKey + '&language=id-id');
   const response_1 = await response.json();
   const weatherText = response_1[0].WeatherText;
+  const dataDay = response_1[0].IsDayTime;
   const temp = response_1[0].Temperature.Metric.Value;
   const icon = response_1[0].WeatherIcon;
-  return { weatherText, temp, icon };
+  return { weatherText, temp, icon, dataDay };
 }
 
-function dataWeather(nmeDaerah, konCuaca, icon, tempCuaca) {
-  return `<div class="card">
-            <div class="back-color"></div>
-              <div class="icons">
-                <img src="img/icons/${icon}.svg" alt=" " />
-              </div>
-              <div class="info">
+function dataWeather(nmeDaerah, konCuaca, icon, tempCuaca, dataDay) {
+  if (dataDay) {
+    updateUI('Day');
+    return `<div class="card">
+              <img src="img/day.svg" alt="" />
+              <div class="weather-container">
+                <div class="icons">
+                  <img src="img/icons/${icon}.svg" alt=" " />
+                </div>
+                <div class="info">
                 <h2>${nmeDaerah}</h2>
                 <h4>${konCuaca}</h4>
                 <h1>${tempCuaca} °C</h1>
+                </div>
               </div>
-          </div>`;
+            </div>`;
+  } else {
+    updateUI('night');
+    return `<div class="card">
+              <img src="img/night.svg" alt="" />
+              <div class="weather-container">
+                <div class="icons">
+                  <img src="img/icons/${icon}.svg" alt=" " />
+                </div>
+                <div class="info night">
+                <h2>${nmeDaerah}</h2>
+                <h4>${konCuaca}</h4>
+                <h1>${tempCuaca} °C</h1>
+                </div>
+              </div>
+            </div>`;
+  }
+}
+function updateUI(time) {
+  const container = document.querySelector('.container');
+
+  if (time == 'Day') {
+    container.classList.remove('night-mode');
+  } else {
+    container.classList.add('night-mode');
+  }
 }
 function loader() {
   document.querySelector('.loader-container').classList.add('fade-out');
